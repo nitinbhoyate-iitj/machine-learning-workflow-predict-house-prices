8829e1d0ae75fdc3eefbc943ae34cb29fd796413
Nitin Bhoyate IITJ <g24ai2030@iitj.ac.in>
2025-09-28 17:19:51 +0530

    Kernel Ridge model training added

---
+++ train2.py
@@ -0,0 +1,10 @@
+from sklearn.kernel_ridge import KernelRidge
+from misc import load_data, preprocess, train_and_evaluate
+
+df = load_data()
+X_train, X_test, y_train, y_test = preprocess(df)
+
+model = KernelRidge(alpha=1.0)
+mse = train_and_evaluate(model, X_train, X_test, y_train, y_test)
+
+print("Kernel Ridge MSE:", mse)
